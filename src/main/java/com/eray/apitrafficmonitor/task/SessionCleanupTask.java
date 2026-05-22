79b2938a411f1f452e516b1c8fbf469f01dc548e
Eray Günlü <88250653+eraygdev@users.noreply.github.com>
2026-05-22 15:57:49 +0300

    Update: New code changes

--- src/main/java/com/eray/apitrafficmonitor/task/SessionCleanupTask.java
+++ src/main/java/com/eray/apitrafficmonitor/task/SessionCleanupTask.java
@@ -1,5 +1,7 @@
 package com.eray.apitrafficmonitor.task;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.Cursor;
 import org.springframework.data.redis.core.ScanOptions;
@@ -12,23 +14,31 @@ import com.eray.apitrafficmonitor.constant.AppConstants;
 @Component
 public class SessionCleanupTask {
 
+    private static final Logger logger = LoggerFactory.getLogger(SessionCleanupTask.class);
+
     @Autowired
     private StringRedisTemplate redisTemplate;
 
     @Scheduled(fixedRate = 60000)
     public void cleanup() {
-
         long count = 0;
-        Cursor<byte[]> cursor = redisTemplate.getConnectionFactory().getConnection().scan(
-            ScanOptions.scanOptions().match(AppConstants.ACTIVE_SESSION_PREFIX + "*").count(1000).build()
-        );
 
-        while(cursor.hasNext()) {
-            cursor.next();
-            count++;
+        Cursor<byte[]> cursor = redisTemplate.executeWithStickyConnection(connection -> connection.keyCommands().scan(
+                ScanOptions.scanOptions()
+                        .match(AppConstants.ACTIVE_SESSION_PREFIX + "*")
+                        .count(1000)
+                        .build()));
+
+        try {
+            while (cursor.hasNext()) {
+                cursor.next();
+                count++;
+            }
+        } finally {
+            cursor.close();
         }
 
         redisTemplate.opsForValue().set(AppConstants.ACTIVE_USERS_KEY, String.valueOf(count));
-
+        logger.info("Active users: {}", count);
     }
 }
